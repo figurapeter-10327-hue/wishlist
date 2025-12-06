b4b63f9672827885c7de95938e8a38fb255bee38
Figura Peti <figurapeter-10327@taszi.hu>
2025-12-06 14:56:02 +0100

    programelkezdése

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MauiPrjekt.MVVM.Model;
+
+namespace MauiPrjekt
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new KezdoPage();
+        }
+    }
+}
